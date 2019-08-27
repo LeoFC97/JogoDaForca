c737d71ecdab59ab67eeec0398d6443d7e3a1b2b
LeoFC97 <leonardo.fc2@outlook.com>
2019-08-27 00:31:41 -0300

    init DomainObjct

---
+++ src/testes/DomainObject.java
@@ -0,0 +1,16 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import domain.ObjetoDominio;
+import domain.ObjetoDominioImpl;
+
+public class DomainObject {
+
+	@Test
+	public void test() {
+		ObjetoDominio mock = new ObjetoDominioImpl((long) 4);
+	}
+}
