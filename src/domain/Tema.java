9c77f21f9faf86b262e9ae753e2f70c4697d1092
carvalhop88 <carvalho_p2@hotmail.com>
2019-08-27 23:41:30 -0300

    Mensagem de commit

---
+++ src/domain/Tema.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Tema extends ObjetoDominioImpl {
+	
+	public Tema(Long id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	private String nome;
+	
+	
+	@Override
+	public long getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
